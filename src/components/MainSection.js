6bc6bc87d4f93adf757e87b95cfec64ae9af8d43
andreapesic <75815474+andreapesic@users.noreply.github.com>
2022-01-07 12:51:30 +0100

    Napravljena komponenta MainSection.js i dodat odgovarajuci css fajl

---
+++ src/components/MainSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import './MainSection.css';
+
+function MainSection() {
+  return (
+    <div className='main-container'>
+      <image src='../assets/img-home.jpg'/>
+      <h1>WELCOME TO LOTUS SPA!</h1>
+      <p>Do something today that your future self will thank you for.</p>
+    </div>
+  );
+}
+
+export default MainSection;
